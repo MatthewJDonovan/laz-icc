0984bef648c95bd3e14e7f845fffe24c5983889b
Andrew McEwen <andrew.mcewen@redfin.com>
2018-04-13 21:39:39 -0700

    added shopify sponsor

--- server.js
+++ server.js
@@ -8,4 +8,10 @@ app.use(history());
 app.use(serveStatic(__dirname))
 var port = process.env.PORT || 5000
 app.listen(port)
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://lazaridis-case-comp.herokuapp.com");
+}, 1200000);
+
 console.log('server started '+ port) 
