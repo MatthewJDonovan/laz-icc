e4dd1e118e24c58e46d1dc0cf7efc136d50bd927
Andrew McEwen <andrew.mcewen96@gmail.com>
2018-08-15 10:49:54 -0400

    changed stuff

--- dist/server.js
+++ dist/server.js
@@ -9,6 +9,3 @@ app.use(serveStatic(__dirname))
 var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started '+ port)
-setInterval(function() {
-  http.get("https://lazaridis-case-comp.herokuapp.com/");
-}, 300000);
