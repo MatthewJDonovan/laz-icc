7ccac8129e65409b0da97ce88397fc019e104924
Andrew McEwen <andrew.mcewen96@gmail.com>
2018-08-15 10:31:34 -0400

    added sponsors

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -15,6 +15,40 @@
       <div class="uk-container">
         <div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid="">
           <div class="uk-width-1-1@m uk-first-column">
+            <h2 class="uk-h2 uk-heading-bullet"><span>Title Sponsor</span></h2>
+          </div>
+        </div>
+        <div class="uk-grid-margin uk-grid" uk-grid="">
+          <div class="uk-width-expand@m uk-width-1-2@s uk-first-column">
+            <div class="uk-margin uk-panel uk-text-center">
+              <div class="image-container">
+                <img src="../assets/td_logo.png" class="el-image logo-image" alt="">
+              </div>
+            </div>
+            <div>
+              <ul class="uk-subnav uk-margin-remove-bottom uk-subnav-divider uk-flex-center" uk-margin="">
+                <li class="el-item uk-first-column">
+                  <a class="el-link" href="https://www.td.com/ca/en/personal-banking/" target="_blank" >Website</a>
+                </li>
+                <li class="el-item">
+                  <a class="el-link" href="https://twitter.com/TD_Canada" target="_blank" >Twitter</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="uk-width-expand@m uk-width-1-2@s">
+          </div>
+          <div class="uk-width-expand@m uk-width-1-2@s">
+          </div>
+          <div class="uk-width-expand@m uk-width-1-2@s">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="uk-section-default uk-section uk-section-large uk-padding-remove-top">
+      <div class="uk-container">
+        <div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid="">
+          <div class="uk-width-1-1@m uk-first-column">
             <h2 class="uk-h2 uk-heading-bullet"><span>Gold Sponsors</span></h2>
           </div>
         </div>
@@ -53,14 +87,29 @@
               </ul>
             </div>
           </div>
-          <div class="uk-width-expand@m uk-width-1-2@s">
+          <div class="uk-width-expand@m uk-width-1-2@s uk-first-column">
+            <div class="uk-margin uk-panel uk-text-center">
+              <div class="image-container">
+                <img src="../assets/hp_logo.png" class="el-image logo-image" alt="">
+              </div>
+            </div>
+            <div>
+              <ul class="uk-subnav uk-margin-remove-bottom uk-subnav-divider uk-flex-center" uk-margin="">
+                <li class="el-item uk-first-column">
+                  <a class="el-link" href="https://www8.hp.com/ca/en/home.html" target="_blank" >Website</a>
+                </li>
+                <li class="el-item">
+                  <a class="el-link" href="https://twitter.com/hp?lang=en" target="_blank" >Twitter</a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="uk-width-expand@m uk-width-1-2@s">
           </div>
         </div>
       </div>
     </div>
-        <div class="uk-section-default uk-section uk-section-large uk-padding-remove-top">
+    <div class="uk-section-default uk-section uk-section-large uk-padding-remove-top">
       <div class="uk-container">
         <div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid="">
           <div class="uk-width-1-1@m uk-first-column">
@@ -136,6 +185,54 @@
               </ul>
             </div>
           </div>
+          <div class="uk-width-expand@m uk-width-1-2@s">
+            <div class="uk-margin uk-panel uk-text-center">
+              <div class="image-container">
+                <img src="../assets/global_logo.png" class="el-image logo-image" alt="">
+              </div>
+            </div>
+            <div>
+              <ul class="uk-subnav uk-margin-remove-bottom uk-subnav-divider uk-flex-center" uk-margin="">
+                <li class="el-item uk-first-column">
+                  <a class="el-link" href="https://www.globalfurnituregroup.com/" target="_blank" >Website</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="uk-section-default uk-section uk-section-large uk-padding-remove-top">
+      <div class="uk-container">
+        <div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid="">
+          <div class="uk-width-1-1@m uk-first-column">
+            <h2 class="uk-h2 uk-heading-bullet"><span>Bronze Sponsors</span></h2>
+          </div>
+        </div>
+        <div class="uk-grid-margin uk-grid" uk-grid="">
+          <div class="uk-width-expand@m uk-width-1-2@s uk-first-column">
+            <div class="uk-margin uk-panel uk-text-center">
+              <div class="image-container">
+                <img src="../assets/fluent.png" class="el-image logo-image" alt="">
+              </div>
+            </div>
+            <div>
+              <ul class="uk-subnav uk-margin-remove-bottom uk-subnav-divider uk-flex-center" uk-margin="">
+                <li class="el-item uk-first-column">
+                  <a class="el-link" href="https://www.fluenthome.com/" target="_blank" >Website</a>
+                </li>
+                <li class="el-item">
+                  <a class="el-link" href="https://twitter.com/fluenthome?lang=en" target="_blank" >Twitter</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="uk-width-expand@m uk-width-1-2@s">
+          </div>
+          <div class="uk-width-expand@m uk-width-1-2@s">
+          </div>
+          <div class="uk-width-expand@m uk-width-1-2@s">
+          </div>
         </div>
       </div>
     </div>
@@ -162,42 +259,42 @@
 </template>
 
 <script>
-  import Footnav from "./Footer.vue";
-  import Navbar from "./Navbar.vue";
-  export default {
-    name: "sponsors",
-    components: {
-      Footnav,
-      Navbar
-    },
-    methods: {
-      toSite: function(url) {
-        var win = window.open(url, "_blank");
-        win.focus();
-      }
+import Footnav from "./Footer.vue";
+import Navbar from "./Navbar.vue";
+export default {
+  name: "sponsors",
+  components: {
+    Footnav,
+    Navbar
+  },
+  methods: {
+    toSite: function(url) {
+      var win = window.open(url, "_blank");
+      win.focus();
     }
-  };
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  .sponsor-image {
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: contain;
-    background-image: url("../assets/d2l_logo.png");
-    max-width: 33%;
-    height: 200px;
-    margin-left: auto;
-    margin-right: auto;
-    cursor: pointer;
-  }
+.sponsor-image {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-image: url("../assets/d2l_logo.png");
+  max-width: 33%;
+  height: 200px;
+  margin-left: auto;
+  margin-right: auto;
+  cursor: pointer;
+}
 
-  .image-container {
+.image-container {
+  max-height: 200px;
+  height: 200px;
+  .logo-image {
     max-height: 200px;
-    height: 200px;
-    .logo-image {
-      max-height: 200px;
-    }
   }
+}
 </style>
