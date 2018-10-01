515e49578aef4cbdf198c09d25e941a3cf7495c0
Andrew McEwen <andrew.mcewen96@gmail.com>
2018-09-30 20:52:20 -0400

    Adding files.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var path = require('path');
+var history = require('connect-history-api-fallback');
+
+var express = require('express')
+var path = require('path')
 var serveStatic = require('serve-static')
-app = express();
+var http = require('http')
+app = express()
+app.use(history());    
 app.use(serveStatic(path.join(__dirname, 'dist')));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started' + port);
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started '+ port)
+
+setInterval(function() {
+  http.get("http://www.lazicc.com");
+}, 300000);
